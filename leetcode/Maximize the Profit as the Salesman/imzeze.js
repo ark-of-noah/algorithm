dc349ba3098268bbb2dcf119cd994b26f81ef957
imzeze <jihyekim019@gmail.com>
2023-09-29 23:45:07 +0900

    feat: maximize profit 풀이

---
+++ leetcode/Maximize the Profit as the Salesman/imzeze.js
@@ -0,0 +1,33 @@
+const maximizeTheProfit = function (n, offers) {
+  let result = 0;
+  const dp = [];
+
+  // end point를 기준으로 정렬
+  // end point까지의 최대 이익을 구하기 위함
+  offers.sort((a, b) => a[1] - b[1]);
+
+  for (offer of offers) {
+    let [start, end, profit] = offer;
+
+    // start point의 이전 point가 0인 경우 start를 이전 point로 조정
+    while (start > 0 && !dp[start - 1]) {
+      start--;
+    }
+
+    // start point가 0인 경우 start point가 0인 다른 offer와 겹칠 수 있으므로 분기 처리 필요
+    if (start > 0) {
+      dp[end] = (dp[start - 1] || 0) + profit;
+    } else {
+      dp[end] = profit;
+    }
+
+    // offer의 end point가 항상 최대값을 갖게 됌 (누적)
+    if (dp[end] < result) {
+      dp[end] = result;
+    } else {
+      result = dp[end];
+    }
+  }
+
+  return result;
+};
