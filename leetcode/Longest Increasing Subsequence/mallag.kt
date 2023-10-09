ae9e0a3c583e8806965b888155ab6a0c6f4efb31
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-10-09 22:22:53 +0900

    feat: Longest Increasing Subsequence

---
+++ leetcode/Longest Increasing Subsequence/mallag.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun lengthOfLIS(nums: IntArray): Int {
+        val dp = IntArray(nums.size) { 1 }
+        var max = 1
+
+        for(i in 1 until nums.size) {
+            for(j in 0 until i) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = maxOf(dp[j] + 1, dp[i])
+                }
+            }
+
+            max = Math.max(max, dp[i])
+        }
+
+        return max
+    }
+}
